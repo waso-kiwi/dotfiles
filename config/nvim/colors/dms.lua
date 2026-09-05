fca1c489fbea7527bd457f97b57da985cb44de09
WasoKiwi <139231691+waso-kiwi@users.noreply.github.com>
2026-09-05 15:47:07 +0900

    Update Fedora desktop configuration

--- config/nvim/colors/dms.lua
+++ config/nvim/colors/dms.lua
@@ -88,7 +88,7 @@ if not base46.theme_tables[theme_name] or base46.theme_tables[theme_name].type ~
 	local builtin = vim.deepcopy(assert(base46.get_builtin_theme(theme_base)))
 	local harmonized = base46.theme_harmonize(builtin, "#da6249", harmony)
 	if settings.matugenTemplateNeovimSetBackground ~= false then
-		harmonized = base46.theme_set_bg(harmonized, "#1a110f")
+		harmonized = base46.theme_set_bg(harmonized, "#fff8f6")
 	end
 
 	base46.theme_tables[theme_name] = harmonized
